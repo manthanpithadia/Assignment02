6b1bb2070c00b0d975fbd5209272f419e6427352
Developer1449 <m.a1449pithadia@gmail.com>
2022-02-06 16:48:31 -0500

    Interface created

---
+++ Assignment02/Conversion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class Conversion
+    {
+        private int temp;
+        Conversion() { Console.WriteLine("Temprature Conversions..."); }
+        Conversion(int temp) { this.temp = temp; }
+
+
+    }
+}
---
+++ Assignment02/IConversion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public interface IConversion
+    {
+        double ConvertCelciusToFahrenheit();
+        double ConvertCelciusToKelvin();
+        double ConvertKelvinToFahrenheit();
+        double ConvertKelvinToCelsius();
+        double ConvertFahrenheitToKelvin();
+        double ConvertFahrenheitToCelsius();
+    }
+}
