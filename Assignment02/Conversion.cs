0610591b2839e249f3f7dc15827c4a8a45cf675d
Developer1449 <m.a1449pithadia@gmail.com>
2022-02-06 17:05:11 -0500

    Temprature conversion methods implemented in Conversion class

--- Assignment02/Conversion.cs
+++ Assignment02/Conversion.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace Assignment02
 {
-    class Conversion
+    public class Conversion : IConversion
     {
         private int temp;
-        Conversion() { Console.WriteLine("Temprature Conversions..."); }
-        Conversion(int temp) { this.temp = temp; }
+        Conversion() { 
+            //Initializing temp value=1
+            temp = 1; 
+        }
+        Conversion(int temp) { 
+            //Initializing temp value with parameterized constructor
+            this.temp = temp; 
+        }
+
+        //Assuming that temprature is in Celcius
+        public double ConvertCelciusToFahrenheit() => (9 / 5) * temp + 32;
 
 
+        //Assuming that temprature is in Celcius
+        public double ConvertCelciusToKelvin() => temp + 273.15;
+
+
+        //Assuming that temprature is in Fahrenheit
+        public double ConvertFahrenheitToCelsius() => (5 / 9) * (temp - 32);
+
+
+        //Assuming that temprature is in Fahrenheit
+        public double ConvertFahrenheitToKelvin() => 273.5 + ((temp - 32.0) * (5.0 / 9.0));
+
+
+        //Assuming that temprature is in Kelvin
+        public double ConvertKelvinToCelsius() => temp - 273.15;
+
+
+        //Assuming that temprature is in Kelvin
+        public double ConvertKelvinToFahrenheit() => ((temp - 273.15) * 1.8) + 32;
     }
 }
