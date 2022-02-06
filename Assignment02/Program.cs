a89fdc86617ecfefd7e41726d6dfb85dedf76060
Developer1449 <m.a1449pithadia@gmail.com>
2022-02-06 16:35:35 -0500

    First Commit

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -6,7 +6,7 @@ namespace Assignment02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Init Git Repository");
         }
     }
 }
